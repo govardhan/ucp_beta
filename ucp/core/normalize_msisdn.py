5d640a2939a18aabaff5e5cfe679dedb7d10e546
Govi <govi@ultivoz.com>
2012-10-28 02:15:42 +0800

    final updates and renaming the file

--- ucp/core/normalize_msisdn.py
+++ ucp/core/normalize_msisdn.py
@@ -6,7 +6,7 @@ import time
 import re
 
 @singleton
-class Normalizer:
+class UVNormalizer:
   def __init__(self):
     self.init()
   
@@ -15,13 +15,13 @@ class Normalizer:
 
   def init(self):
     self.db_name = UVConfig().get_config_value("database","db_name.core") 
-    self.noofrule, self.normalize_rules = DBPool().execute_query("select id, in_pattern, out_pattern, telco_id, channel, description from tb_number_normalizer order by id desc", self.db_name)
+    self.rowcount, self.normalize_rules = DBPool().execute_query("select id, in_pattern, out_pattern, telco_id, channel, remarks from tb_number_normalizer order by id desc", self.db_name)
 
     logging.info("Normalized rules in search order top to bottom")
-    logging.info("id	in_pattern	out_pattern	telco_id	channel		desc")
+    logging.info("id	in_pattern	out_pattern	telco_id	channel		remarks")
     logging.info("-" * 70)
     for l_row in self.normalize_rules:
-      logging.info("{0}\t{1}\t{2}\t{3}\t{4}\t{5}".format(l_row['id'], l_row['in_pattern'], l_row['out_pattern'], l_row['telco_id'], l_row['channel'], l_row['description']))
+      logging.info("{0}\t{1}\t{2}\t{3}\t{4}\t{5}".format(l_row['id'], l_row['in_pattern'], l_row['out_pattern'], l_row['telco_id'], l_row['channel'], l_row['remarks']))
 
   def normalize(self, p_msisdn, p_telco_id = ".*", p_channel = ".*"):
     logging.debug("params - p_msisdn {0}, p_telco_id {1}, p_channel {2}".format(p_msisdn, p_telco_id, p_channel))
@@ -32,7 +32,7 @@ class Normalizer:
         return True, l_norm_msisdn
 
     #End of for loop. No match found. So return False
-    logging.info("No normalizer match not found. p_msisdn = {0}, p_telco_id = {1}. p_channel = {2}".format(p_msisdn, p_telco_id, p_channel) )
+    logging.warn("No normalizer match not found. p_msisdn = {0}, p_telco_id = {1}. p_channel = {2}".format(p_msisdn, p_telco_id, p_channel) )
     return False, p_msisdn
 
 #Run unit tests
@@ -41,7 +41,7 @@ if __name__ == "__main__":
   conf = UVConfig()
   conf.init("/root/ucp/ucp/conf/ucp.conf")
 
-  l_normalizer = Normalizer()
+  l_normalizer = UVNormalizer()
   l_found, l_result = l_normalizer.normalize("9886161856")
   l_found, l_result = l_normalizer.normalize("9886161856", p_telco_id = "91.*")
 
